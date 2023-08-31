7fc7c9a662f06f2382f982c1796f61cd09246e75
fedeminatta <minattafederico@gmail.com>
2023-08-31 15:10:27 -0300

    css

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -3,86 +3,82 @@ import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 const ControlPresupuesto = ({
-	gastos,
-	setGastos,
-	presupuesto,
-	setPresupuesto,
-	setIsValidPresupuesto,
+  gastos,
+  setGastos,
+  presupuesto,
+  setPresupuesto,
+  setIsValidPresupuesto,
 }) => {
-	const [porcentaje, setPorcentaje] = useState(0);
-	const [disponible, setDisponible] = useState(0);
-	const [gastado, setGastado] = useState(0);
+  const [porcentaje, setPorcentaje] = useState(0);
+  const [disponible, setDisponible] = useState(0);
+  const [gastado, setGastado] = useState(0);
 
-	useEffect(() => {
-		const totalGastado = gastos.reduce((total, gasto) => {
-			return gasto.cantidad + total;
-		}, 0);
+  useEffect(() => {
+    const totalGastado = gastos.reduce((total, gasto) => {
+      return gasto.cantidad + total;
+    }, 0);
 
-		const totalDisponible = presupuesto - totalGastado;
+    const totalDisponible = presupuesto - totalGastado;
 
-		// Calcular el % gastado
-		const nuevoPorcentaje = (
-			((presupuesto - totalDisponible) / presupuesto) *
-			100
-		).toFixed(2);
+    // Calcular el % gastado
+    const nuevoPorcentaje = (
+      ((presupuesto - totalDisponible) / presupuesto) *
+      100
+    ).toFixed(2);
 
-		setDisponible(totalDisponible);
-		setGastado(totalGastado);
+    setDisponible(totalDisponible);
+    setGastado(totalGastado);
 
-		setTimeout(() => {
-			setPorcentaje(nuevoPorcentaje);
-		}, 1500);
-	}, [gastos]);
+    setTimeout(() => {
+      setPorcentaje(nuevoPorcentaje);
+    }, 1500);
+  }, [gastos]);
 
-	const formatearCantidad = (cantidad) => {
-		return cantidad.toLocaleString('en-US', {
-			style: 'currency',
-			currency: 'USD',
-		});
-	};
+  const formatearCantidad = (cantidad) => {
+    return cantidad.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+  };
 
-	const handleResetApp = () => {
-		const result = confirm('¿Deseas Reiniciar Presupuesto y Gastos?');
+  const handleResetApp = () => {
+    const result = confirm('¿Deseas Reiniciar Presupuesto y Gastos?');
 
-		if (result) {
-			setGastos([]);
-			setPresupuesto(0);
-			setIsValidPresupuesto(false);
-		}
-	};
+    if (result) {
+      setGastos([]);
+      setPresupuesto(0);
+      setIsValidPresupuesto(false);
+    }
+  };
 
-	return (
-		<div className="contenedor-presupuesto contenedor sombra dos-columnas">
-			<div>
-				<CircularProgressbar
-					value={porcentaje}
-					styles={buildStyles({
-						pathColor: porcentaje > 100 ? '#DC2626' : '#3b82f6',
-						trailColor: '#f5f5f5',
-						textColor: porcentaje > 100 ? '#DC2626' : '#3b82f6',
-					})}
-					text={`${porcentaje}% Gastado`}
-				/>
-			</div>
-			<div className="contenido-presupuesto">
-				<button
-					className="reset-app"
-					type="button"
-					onClick={handleResetApp}
-				>
-					Resetear App
-				</button>
-				<p>
-					<span>Presupuesto: </span> {formatearCantidad(presupuesto)}
-				</p>
-				<p className={`${disponible < 0 ? 'negativo' : ''}`}>
-					<span>Disponible: </span> {formatearCantidad(disponible)}
-				</p>
-				<p>
-					<span>Gastado: </span> {formatearCantidad(gastado)}
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className="contenedor-presupuesto contenedor sombra dos-columnas">
+      <div>
+        <CircularProgressbar
+          value={porcentaje}
+          styles={buildStyles({
+            pathColor: porcentaje > 100 ? '#DC2626' : '#d4a257',
+            trailColor: '#f5f5f5',
+            textColor: porcentaje > 100 ? '#DC2626' : '#d4a257',
+          })}
+          text={`${porcentaje}% Gastado`}
+        />
+      </div>
+      <div className="contenido-presupuesto">
+        <button className="reset-app" type="button" onClick={handleResetApp}>
+          Resetear App
+        </button>
+        <p>
+          <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
+        </p>
+        <p className={`${disponible < 0 ? 'negativo' : ''}`}>
+          <span>Disponible: </span> {formatearCantidad(disponible)}
+        </p>
+        <p>
+          <span>Gastado: </span> {formatearCantidad(gastado)}
+        </p>
+      </div>
+    </div>
+  );
 };
 export default ControlPresupuesto;
